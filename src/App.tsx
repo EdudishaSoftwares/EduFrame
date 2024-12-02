aeeea181da783110e8901265a9c65c76e3b693a9
prokrishpatel <pro.krishpatel@gmail.com>
2024-12-02 22:30:18 +0530

    build fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
-import { Routes, Route } from 'react-router-dom';
 import envCongfig from './environments/environment';
 function App() {
   document.title = envCongfig.title;
 
   return (
-    <Routes>
-    </Routes>
+    <div>
+      <h1>Hello, Edusdisha Dev team we will be live soon</h1>
+    </div>
   )
 }
 
