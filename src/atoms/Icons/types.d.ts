1ffff78d51258161fcdad0137392d4b9f77ae599
prokrishpatel <pro.krishpatel@gmail.com>
2024-12-22 19:01:18 +0530

    build update

--- src/atoms/Icons/types.d.ts
+++ src/atoms/Icons/types.d.ts
@@ -1,8 +1,10 @@
+import { Colors } from "../../typings";
 export type IconProps = {
   width?: number;
   height?: number;
   fillPath?: string;
   contentGap?: 'sm' | 'md' | 'lg';
   align?: 'top' | 'middle' | 'bottom';
-  stroke?: string;
+  fill?: Colors;
+  stroke?: Colors;
 }
